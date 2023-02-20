43a15ad40bdd9ee1510a7ea12981fe1b33f21bd6
YasinCoban123 <9018681@student.zadkine.nl>
2023-02-20 23:20:12 +0100

    rcfrf

--- opdracht13/MaakGebruiker.php
+++ opdracht13/MaakGebruiker.php
@@ -1,23 +1,17 @@
-<?php
+<?php 
+
 try {
-    $db = new PDO("mysql:host=localhost;dbname=fietsenmaker", "root", "");
-} catch (PDOException $e) {
-    die("Error!: " . $e->getMessage());
-}
+  $db = new PDO("mysql:host=localhost;dbname=fietsenmaker","root","");
+  $password = password_hash("geheim", PASSWORD_DEFAULT);
 
-
-$username = "ik";
-$password = password_hash("geheim", PASSWORD_DEFAULT);
-
-
-$query = $db->prepare("INSERT INTO gebruikers (username, password) VALUES (:username, :password)");
-$query->bindParam(":username", $username);
-$query->bindParam(":password", $password);
-
-
-if ($query->execute()) {
-    echo "De nieuwe gegevens zijn toegevoegd.";
-} else {
+  $query = $db->prepare("INSERT INTO gebruikers(username, password) VALUES ('ik', '" . $password . "')");
+  if ($query->execute()){
+    echo "De nieuwe gegevens zijn toegevoegd. ";
+  } else{
     echo "Er is een fout opgetreden!";
+  }
+} catch(PDOException $e){
+  die("Error!: " . $e->getMessage());
 }
+
 ?>
